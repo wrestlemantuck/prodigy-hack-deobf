31ba48c35a82c910a9d9bbe617ccae73cd1dc508
wrestlemantuck <wrestlemantuck@gmail.com>
2024-09-27 21:23:52 -0500

    Create walk-anywhere-deobfuscated.js

---
+++ walk-anywhere-deobfuscated.js
@@ -0,0 +1,71 @@
+var _0x7f0d81 = (function () {
+  var _0x2db34a = true
+  return function (_0x9cedc0, _0x14aef9) {
+    var _0x1dfff4 = _0x2db34a
+      ? function () {
+          if (_0x14aef9) {
+            var _0x1951c0 = _0x14aef9.apply(_0x9cedc0, arguments)
+            return (_0x14aef9 = null), _0x1951c0
+          }
+        }
+      : function () {}
+    return (_0x2db34a = false), _0x1dfff4
+  }
+})()
+var _0x1f4027 = _0x7f0d81(this, function () {
+  return _0x1f4027
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(_0x1f4027)
+    .search('(((.+)+)+)+$')
+})
+_0x1f4027()
+var _0x7db0aa = (function () {
+  var _0x472c37 = true
+  return function (_0x97707, _0x449c74) {
+    var _0x1d0f5f = _0x472c37
+      ? function () {
+          if (_0x449c74) {
+            var _0x257c00 = _0x449c74.apply(_0x97707, arguments)
+            return (_0x449c74 = null), _0x257c00
+          }
+        }
+      : function () {}
+    return (_0x472c37 = false), _0x1d0f5f
+  }
+})()
+var _0x4f720c = _0x7db0aa(this, function () {
+  var _0x2a88a1
+  try {
+    var _0xf0637c = Function(
+      'return (function() {}.constructor("return this")( ));'
+    )
+    _0x2a88a1 = _0xf0637c()
+  } catch (_0x2082d5) {
+    _0x2a88a1 = window
+  }
+  var _0x305a1a = (_0x2a88a1.console = _0x2a88a1.console || {})
+  var _0x58653c = [
+    'log',
+    'warn',
+    'info',
+    'error',
+    'exception',
+    'table',
+    'trace',
+  ]
+  for (var _0x9283ea = 0; _0x9283ea < _0x58653c.length; _0x9283ea++) {
+    var _0x53954b = _0x7db0aa.constructor.prototype.bind(_0x7db0aa)
+    var _0x28c860 = _0x58653c[_0x9283ea]
+    var _0x535b9b = _0x305a1a[_0x28c860] || _0x53954b
+    _0x53954b['__proto__'] = _0x7db0aa.bind(_0x7db0aa)
+    _0x53954b.toString = _0x535b9b.toString.bind(_0x535b9b)
+    _0x305a1a[_0x28c860] = _0x53954b
+  }
+})
+_0x4f720c()
+var area = Boot.prototype.game['_state']['_current'].area
+for (var i = 0; i < area.length; i++) {
+  area[i] = Array(64).fill(1)
+}
