73fa0576f047ba77a8ea7352f301b67ee9c876e2
wrestlemantuck <wrestlemantuck@gmail.com>
2024-09-27 21:25:06 -0500

    Create damage-hack-deobfuscated.js

---
+++ damage-hack-deobfuscated.js
@@ -0,0 +1,70 @@
+var _0x15d043 = (function () {
+  var _0x1831e6 = true
+  return function (_0x4185c6, _0x242eac) {
+    var _0x5bcf4a = _0x1831e6
+      ? function () {
+          if (_0x242eac) {
+            var _0x7134c3 = _0x242eac.apply(_0x4185c6, arguments)
+            return (_0x242eac = null), _0x7134c3
+          }
+        }
+      : function () {}
+    return (_0x1831e6 = false), _0x5bcf4a
+  }
+})()
+var _0x5421d2 = _0x15d043(this, function () {
+  return _0x5421d2
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(_0x5421d2)
+    .search('(((.+)+)+)+$')
+})
+_0x5421d2()
+var _0x3aba75 = (function () {
+    var _0x39dc1c = true
+    return function (_0x22120d, _0x2a6c9f) {
+      var _0x438898 = _0x39dc1c
+        ? function () {
+            if (_0x2a6c9f) {
+              var _0x4d2d24 = _0x2a6c9f.apply(_0x22120d, arguments)
+              return (_0x2a6c9f = null), _0x4d2d24
+            }
+          }
+        : function () {}
+      return (_0x39dc1c = false), _0x438898
+    }
+  })(),
+  _0x37851f = _0x3aba75(this, function () {
+    var _0x45a19e
+    try {
+      var _0x524b86 = Function(
+        'return (function() {}.constructor("return this")( ));'
+      )
+      _0x45a19e = _0x524b86()
+    } catch (_0xf38c2a) {
+      _0x45a19e = window
+    }
+    var _0x2bcfb7 = (_0x45a19e.console = _0x45a19e.console || {}),
+      _0x793ac2 = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'exception',
+        'table',
+        'trace',
+      ]
+    for (var _0x49d04c = 0; _0x49d04c < _0x793ac2.length; _0x49d04c++) {
+      var _0x14cedd = _0x3aba75.constructor.prototype.bind(_0x3aba75)
+      var _0x5837c0 = _0x793ac2[_0x49d04c]
+      var _0xc23096 = _0x2bcfb7[_0x5837c0] || _0x14cedd
+      _0x14cedd['__proto__'] = _0x3aba75.bind(_0x3aba75)
+      _0x14cedd.toString = _0xc23096.toString.bind(_0xc23096)
+      _0x2bcfb7[_0x5837c0] = _0x14cedd
+    }
+  })
+_0x37851f()
+alert('Made by rxzyx (rzx)')
+Boot.prototype.game['_state']['_current'].user.source.modifiers.damage =
+  Infinity
