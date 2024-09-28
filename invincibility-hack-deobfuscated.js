bff8d26358971a2aabe3d65da35bd8781210c9fe
wrestlemantuck <wrestlemantuck@gmail.com>
2024-09-27 21:26:17 -0500

    Create invincibility-hack-deobfuscated.js

---
+++ invincibility-hack-deobfuscated.js
@@ -0,0 +1,71 @@
+var _0x5702bc = (function () {
+    var _0x17c7ac = true
+    return function (_0x3ac239, _0x485071) {
+      var _0x220b7a = _0x17c7ac
+        ? function () {
+            if (_0x485071) {
+              var _0x39340a = _0x485071.apply(_0x3ac239, arguments)
+              return (_0x485071 = null), _0x39340a
+            }
+          }
+        : function () {}
+      return (_0x17c7ac = false), _0x220b7a
+    }
+  })(),
+  _0x447b73 = _0x5702bc(this, function () {
+    return _0x447b73
+      .toString()
+      .search('(((.+)+)+)+$')
+      .toString()
+      .constructor(_0x447b73)
+      .search('(((.+)+)+)+$')
+  })
+_0x447b73()
+var _0x5efcc1 = (function () {
+    var _0x18b052 = true
+    return function (_0x31aa85, _0x2f9eec) {
+      var _0x6a5736 = _0x18b052
+        ? function () {
+            if (_0x2f9eec) {
+              var _0x221385 = _0x2f9eec.apply(_0x31aa85, arguments)
+              return (_0x2f9eec = null), _0x221385
+            }
+          }
+        : function () {}
+      return (_0x18b052 = false), _0x6a5736
+    }
+  })(),
+  _0x4c0ea5 = _0x5efcc1(this, function () {
+    var _0xb9572c = function () {
+      var _0x1e3ea6
+      try {
+        _0x1e3ea6 = Function(
+          'return (function() {}.constructor("return this")( ));'
+        )()
+      } catch (_0xc3b8f4) {
+        _0x1e3ea6 = window
+      }
+      return _0x1e3ea6
+    }
+    var _0x51eb24 = _0xb9572c()
+    var _0x350bbb = (_0x51eb24.console = _0x51eb24.console || {}),
+      _0x20a2a2 = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'exception',
+        'table',
+        'trace',
+      ]
+    for (var _0x33d84c = 0; _0x33d84c < _0x20a2a2.length; _0x33d84c++) {
+      var _0x561e8b = _0x5efcc1.constructor.prototype.bind(_0x5efcc1)
+      var _0x36a6e9 = _0x20a2a2[_0x33d84c]
+      var _0x928f34 = _0x350bbb[_0x36a6e9] || _0x561e8b
+      _0x561e8b['__proto__'] = _0x5efcc1.bind(_0x5efcc1)
+      _0x561e8b.toString = _0x928f34.toString.bind(_0x928f34)
+      _0x350bbb[_0x36a6e9] = _0x561e8b
+    }
+  })
+_0x4c0ea5()
+Boot.prototype.game['_state']['_states'].get('Battle').mods.invincible = true
