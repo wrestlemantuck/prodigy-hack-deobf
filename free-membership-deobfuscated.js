0b0395dcce8bae5f8eabd078f54180109ede4f32
wrestlemantuck <wrestlemantuck@gmail.com>
2024-09-27 21:12:48 -0500

    Create free-membership-deobfuscated.js

---
+++ free-membership-deobfuscated.js
@@ -0,0 +1,72 @@
+var _0x44d58d = (function () {
+  var _0x319ead = true
+  return function (_0x1ce1ae, _0x19653d) {
+    var _0x2ef27a = _0x319ead
+      ? function () {
+          if (_0x19653d) {
+            var _0x1d8b20 = _0x19653d.apply(_0x1ce1ae, arguments)
+            return (_0x19653d = null), _0x1d8b20
+          }
+        }
+      : function () {}
+    return (_0x319ead = false), _0x2ef27a
+  }
+})()
+var _0x3d6262 = _0x44d58d(this, function () {
+  return _0x3d6262
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(_0x3d6262)
+    .search('(((.+)+)+)+$')
+})
+_0x3d6262()
+var _0x2342f1 = (function () {
+    var _0x105003 = true
+    return function (_0x1d70be, _0x316bc3) {
+      var _0x5ceef6 = _0x105003
+        ? function () {
+            if (_0x316bc3) {
+              var _0x304414 = _0x316bc3.apply(_0x1d70be, arguments)
+              return (_0x316bc3 = null), _0x304414
+            }
+          }
+        : function () {}
+      return (_0x105003 = false), _0x5ceef6
+    }
+  })(),
+  _0x7a3c59 = _0x2342f1(this, function () {
+    var _0xb44075
+    try {
+      var _0x537edf = Function(
+        'return (function() {}.constructor("return this")( ));'
+      )
+      _0xb44075 = _0x537edf()
+    } catch (_0x239f61) {
+      _0xb44075 = window
+    }
+    var _0x5f249b = (_0xb44075.console = _0xb44075.console || {}),
+      _0x580dc2 = [
+        'log',
+        'warn',
+        'info',
+        'error',
+        'exception',
+        'table',
+        'trace',
+      ]
+    for (var _0x4242f7 = 0; _0x4242f7 < _0x580dc2.length; _0x4242f7++) {
+      var _0x2339bc = _0x2342f1.constructor.prototype.bind(_0x2342f1)
+      var _0x574b27 = _0x580dc2[_0x4242f7]
+      var _0x1391fe = _0x5f249b[_0x574b27] || _0x2339bc
+      _0x2339bc['__proto__'] = _0x2342f1.bind(_0x2342f1)
+      _0x2339bc.toString = _0x1391fe.toString.bind(_0x1391fe)
+      _0x5f249b[_0x574b27] = _0x2339bc
+    }
+  })
+_0x7a3c59()
+Boot.prototype.game['_state']['_current'].user.source.hasMembership =
+  function () {
+    return true
+  }
+Boot.prototype.game['_state']['_current'].user.source.appearanceChanged = true
