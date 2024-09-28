426b62ac9539b06144c45300179fe7f820544c74
wrestlemantuck <wrestlemantuck@gmail.com>
2024-09-27 21:19:25 -0500

    Create unlimited-spins-deobfuscated.js

---
+++ unlimited-spins-deobfuscated.js
@@ -0,0 +1,70 @@
+var _0x250a1f = (function () {
+    var _0x105b20 = true
+    return function (_0x572701, _0x3ada97) {
+      var _0x305eec = _0x105b20
+        ? function () {
+            if (_0x3ada97) {
+              var _0x39d448 = _0x3ada97.apply(_0x572701, arguments)
+              return (_0x3ada97 = null), _0x39d448
+            }
+          }
+        : function () {}
+      return (_0x105b20 = false), _0x305eec
+    }
+  })(),
+  _0x35621f = _0x250a1f(this, function () {
+    return _0x35621f
+      .toString()
+      .search('(((.+)+)+)+$')
+      .toString()
+      .constructor(_0x35621f)
+      .search('(((.+)+)+)+$')
+  })
+_0x35621f()
+var _0x3fe775 = (function () {
+  var _0x314ea6 = true
+  return function (_0x24b534, _0x4c27ee) {
+    var _0x42b319 = _0x314ea6
+      ? function () {
+          if (_0x4c27ee) {
+            var _0x75d2af = _0x4c27ee.apply(_0x24b534, arguments)
+            return (_0x4c27ee = null), _0x75d2af
+          }
+        }
+      : function () {}
+    return (_0x314ea6 = false), _0x42b319
+  }
+})()
+var _0x240ef3 = _0x3fe775(this, function () {
+  var _0x414950
+  try {
+    var _0x39d455 = Function(
+      'return (function() {}.constructor("return this")( ));'
+    )
+    _0x414950 = _0x39d455()
+  } catch (_0x189f3e) {
+    _0x414950 = window
+  }
+  var _0x54f499 = (_0x414950.console = _0x414950.console || {})
+  var _0xfa2fff = [
+    'log',
+    'warn',
+    'info',
+    'error',
+    'exception',
+    'table',
+    'trace',
+  ]
+  for (var _0x417f0b = 0; _0x417f0b < _0xfa2fff.length; _0x417f0b++) {
+    var _0x30dd19 = _0x3fe775.constructor.prototype.bind(_0x3fe775)
+    var _0x4125a6 = _0xfa2fff[_0x417f0b]
+    var _0x2d183a = _0x54f499[_0x4125a6] || _0x30dd19
+    _0x30dd19['__proto__'] = _0x3fe775.bind(_0x3fe775)
+    _0x30dd19.toString = _0x2d183a.toString.bind(_0x2d183a)
+    _0x54f499[_0x4125a6] = _0x30dd19
+  }
+})
+_0x240ef3()
+Boot.prototype.game['_state']['_current'].user.source.canSpin = function () {
+  return true
+}
