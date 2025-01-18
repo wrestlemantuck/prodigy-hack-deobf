27f49c2bacad595ee4f2ef781b3ed5bf1b2cd7cc
wrestlemantuck <wrestlemantuck@gmail.com>
2025-01-18 16:21:39 -0600

    Fixxed vars

--- fill-energy-deobfuscated.js
+++ fill-energy-deobfuscated.js
@@ -1,80 +1,77 @@
-var _0x43142c = (function () {
-    var _0x104d44 = true
-    return function (_0x3ae950, _0x3532c4) {
-      var _0x4ef1ee = _0x104d44
-        ? function () {
-            if (_0x3532c4) {
-              var _0x569ab1 = _0x3532c4.apply(_0x3ae950, arguments)
-              return (_0x3532c4 = null), _0x569ab1
-            }
+var oneTimeFunction = (function () {
+  var isFirstCall = true;
+  return function (context, callback) {
+    var executeOnce = isFirstCall
+      ? function () {
+          if (callback) {
+            var result = callback.apply(context, arguments);
+            return (callback = null), result;
           }
-        : function () {}
-      return (_0x104d44 = false), _0x4ef1ee
-    }
-  })(),
-  _0x20e440 = _0x43142c(this, function () {
-    return _0x20e440
-      .toString()
-      .search('(((.+)+)+)+$')
-      .toString()
-      .constructor(_0x20e440)
-      .search('(((.+)+)+)+$')
-  })
-_0x20e440()
-var _0x2e665e = (function () {
-    var _0x54ee31 = true
-    return function (_0x3663e6, _0xa58016) {
-      var _0x150809 = _0x54ee31
-        ? function () {
-            if (_0xa58016) {
-              var _0x7809b = _0xa58016.apply(_0x3663e6, arguments)
-              return (_0xa58016 = null), _0x7809b
-            }
+        }
+      : function () {};
+    return (isFirstCall = false), executeOnce;
+  };
+})();
+var searchFunction = oneTimeFunction(this, function () {
+  return searchFunction
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(searchFunction)
+    .search('(((.+)+)+)+$');
+});
+searchFunction();
+
+var createConsoleMock = (function () {
+  var isFirstCall = true;
+  return function (context, callback) {
+    var executeOnce = isFirstCall
+      ? function () {
+          if (callback) {
+            var result = callback.apply(context, arguments);
+            return (callback = null), result;
           }
-        : function () {}
-      return (_0x54ee31 = false), _0x150809
+        }
+      : function () {};
+    return (isFirstCall = false), executeOnce;
+  };
+})();
+
+var enhanceConsoleMethods = createConsoleMock(this, function () {
+  var getGlobalContext = function () {
+    var context;
+    try {
+      context = Function(
+        'return (function() {}.constructor("return this")( ));'
+      )();
+    } catch (error) {
+      context = window;
     }
-  })(),
-  _0x4d63b2 = _0x2e665e(this, function () {
-    var _0x297dba = function () {
-      var _0x3fb399
-      try {
-        _0x3fb399 = Function(
-          'return (function() {}.constructor("return this")( ));'
-        )()
-      } catch (_0x1880b7) {
-        _0x3fb399 = window
-      }
-      return _0x3fb399
-    }
-    var _0x85613d = _0x297dba(),
-      _0x511d83 = (_0x85613d.console = _0x85613d.console || {}),
-      _0x513cdc = [
-        'log',
-        'warn',
-        'info',
-        'error',
-        'exception',
-        'table',
-        'trace',
-      ]
-    for (var _0x19b0ab = 0; _0x19b0ab < _0x513cdc.length; _0x19b0ab++) {
-      var _0xf14a0f = _0x2e665e.constructor.prototype.bind(_0x2e665e)
-      var _0x20962c = _0x513cdc[_0x19b0ab]
-      var _0x400edf = _0x511d83[_0x20962c] || _0xf14a0f
-      _0xf14a0f['__proto__'] = _0x2e665e.bind(_0x2e665e)
-      _0xf14a0f.toString = _0x400edf.toString.bind(_0x400edf)
-      _0x511d83[_0x20962c] = _0xf14a0f
-    }
-  })
-_0x4d63b2()
-setInterval(function () {
-  var _0x11e630 = Boot.prototype.game['_state']['_current'].currentManager
-  if (
-    _0x11e630 &&
-    _0x11e630['_playerTeam'] !== void 0 &&
-    _0x11e630['_playerTeam'] !== null
-  ) {
-    _0x11e630['_playerTeam'].currentEnergy = _0x11e630['_playerTeam'].maxEnergy
+    return context;
+  };
+  var globalContext = getGlobalContext();
+
+  var consoleMethods = (globalContext.console = globalContext.console || {}),
+    logMethods = ['log', 'warn', 'info', 'error', 'exception', 'table', 'trace'];
+
+  for (var i = 0; i < logMethods.length; i++) {
+    var bindFunction = createConsoleMock.constructor.prototype.bind(createConsoleMock);
+    var method = logMethods[i];
+    var originalMethod = consoleMethods[method] || bindFunction;
+    bindFunction['__proto__'] = createConsoleMock.bind(createConsoleMock);
+    bindFunction.toString = originalMethod.toString.bind(originalMethod);
+    consoleMethods[method] = bindFunction;
   }
-})
+});
+enhanceConsoleMethods();
+
+setInterval(function () {
+  var currentManager = Boot.prototype.game['_state']['_current'].currentManager;
+  if (
+    currentManager &&
+    currentManager['_playerTeam'] !== void 0 &&
+    currentManager['_playerTeam'] !== null
+  ) {
+    currentManager['_playerTeam'].currentEnergy = currentManager['_playerTeam'].maxEnergy;
+  }
+});
