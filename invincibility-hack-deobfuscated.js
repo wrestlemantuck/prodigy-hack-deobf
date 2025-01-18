cf3a4e731133fa3ddcc7eb16f73f0472bc2e52e5
wrestlemantuck <wrestlemantuck@gmail.com>
2025-01-18 16:22:40 -0600

    Fixxed vars

--- invincibility-hack-deobfuscated.js
+++ invincibility-hack-deobfuscated.js
@@ -1,71 +1,68 @@
-var _0x5702bc = (function () {
-    var _0x17c7ac = true
-    return function (_0x3ac239, _0x485071) {
-      var _0x220b7a = _0x17c7ac
-        ? function () {
-            if (_0x485071) {
-              var _0x39340a = _0x485071.apply(_0x3ac239, arguments)
-              return (_0x485071 = null), _0x39340a
-            }
+var firstExecution = (function () {
+  var isFirstCall = true;
+  return function (context, callback) {
+    var executeOnce = isFirstCall
+      ? function () {
+          if (callback) {
+            var result = callback.apply(context, arguments);
+            return (callback = null), result;
           }
-        : function () {}
-      return (_0x17c7ac = false), _0x220b7a
-    }
-  })(),
-  _0x447b73 = _0x5702bc(this, function () {
-    return _0x447b73
-      .toString()
-      .search('(((.+)+)+)+$')
-      .toString()
-      .constructor(_0x447b73)
-      .search('(((.+)+)+)+$')
-  })
-_0x447b73()
-var _0x5efcc1 = (function () {
-    var _0x18b052 = true
-    return function (_0x31aa85, _0x2f9eec) {
-      var _0x6a5736 = _0x18b052
-        ? function () {
-            if (_0x2f9eec) {
-              var _0x221385 = _0x2f9eec.apply(_0x31aa85, arguments)
-              return (_0x2f9eec = null), _0x221385
-            }
+        }
+      : function () {};
+    return (isFirstCall = false), executeOnce;
+  };
+})();
+var searchExecution = firstExecution(this, function () {
+  return searchExecution
+    .toString()
+    .search('(((.+)+)+)+$')
+    .toString()
+    .constructor(searchExecution)
+    .search('(((.+)+)+)+$');
+});
+searchExecution();
+
+var createConsoleMock = (function () {
+  var isFirstCall = true;
+  return function (context, callback) {
+    var executeOnce = isFirstCall
+      ? function () {
+          if (callback) {
+            var result = callback.apply(context, arguments);
+            return (callback = null), result;
           }
-        : function () {}
-      return (_0x18b052 = false), _0x6a5736
+        }
+      : function () {};
+    return (isFirstCall = false), executeOnce;
+  };
+})();
+
+var enhanceConsoleMethods = createConsoleMock(this, function () {
+  var getGlobalContext = function () {
+    var context;
+    try {
+      context = Function(
+        'return (function() {}.constructor("return this")( ));'
+      )();
+    } catch (error) {
+      context = window;
     }
-  })(),
-  _0x4c0ea5 = _0x5efcc1(this, function () {
-    var _0xb9572c = function () {
-      var _0x1e3ea6
-      try {
-        _0x1e3ea6 = Function(
-          'return (function() {}.constructor("return this")( ));'
-        )()
-      } catch (_0xc3b8f4) {
-        _0x1e3ea6 = window
-      }
-      return _0x1e3ea6
-    }
-    var _0x51eb24 = _0xb9572c()
-    var _0x350bbb = (_0x51eb24.console = _0x51eb24.console || {}),
-      _0x20a2a2 = [
-        'log',
-        'warn',
-        'info',
-        'error',
-        'exception',
-        'table',
-        'trace',
-      ]
-    for (var _0x33d84c = 0; _0x33d84c < _0x20a2a2.length; _0x33d84c++) {
-      var _0x561e8b = _0x5efcc1.constructor.prototype.bind(_0x5efcc1)
-      var _0x36a6e9 = _0x20a2a2[_0x33d84c]
-      var _0x928f34 = _0x350bbb[_0x36a6e9] || _0x561e8b
-      _0x561e8b['__proto__'] = _0x5efcc1.bind(_0x5efcc1)
-      _0x561e8b.toString = _0x928f34.toString.bind(_0x928f34)
-      _0x350bbb[_0x36a6e9] = _0x561e8b
-    }
-  })
-_0x4c0ea5()
-Boot.prototype.game['_state']['_states'].get('Battle').mods.invincible = true
+    return context;
+  };
+  var globalContext = getGlobalContext();
+
+  var consoleMethods = (globalContext.console = globalContext.console || {}),
+    logMethods = ['log', 'warn', 'info', 'error', 'exception', 'table', 'trace'];
+
+  for (var i = 0; i < logMethods.length; i++) {
+    var bindFunction = createConsoleMock.constructor.prototype.bind(createConsoleMock);
+    var method = logMethods[i];
+    var originalMethod = consoleMethods[method] || bindFunction;
+    bindFunction['__proto__'] = createConsoleMock.bind(createConsoleMock);
+    bindFunction.toString = originalMethod.toString.bind(originalMethod);
+    consoleMethods[method] = bindFunction;
+  }
+});
+enhanceConsoleMethods();
+
+Boot.prototype.game['_state']['_states'].get('Battle').mods.invincible = true;
