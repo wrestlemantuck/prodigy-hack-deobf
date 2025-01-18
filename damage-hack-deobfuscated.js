16f66aaa3428ea937b41960fbceab6fbef284941
wrestlemantuck <wrestlemantuck@gmail.com>
2025-01-18 16:18:22 -0600

    Fixxed vars

--- damage-hack-deobfuscated.js
+++ damage-hack-deobfuscated.js
@@ -1,70 +1,71 @@
-var _0x15d043 = (function () {
-  var _0x1831e6 = true
-  return function (_0x4185c6, _0x242eac) {
-    var _0x5bcf4a = _0x1831e6
+var createOneTimeFunction = (function () {
+  var isFirstCall = true
+  return function (context, callback) {
+    var onceFunction = isFirstCall
       ? function () {
-          if (_0x242eac) {
-            var _0x7134c3 = _0x242eac.apply(_0x4185c6, arguments)
-            return (_0x242eac = null), _0x7134c3
+          if (callback) {
+            var result = callback.apply(context, arguments)
+            return (callback = null), result
           }
         }
       : function () {}
-    return (_0x1831e6 = false), _0x5bcf4a
+    return (isFirstCall = false), onceFunction
   }
 })()
-var _0x5421d2 = _0x15d043(this, function () {
-  return _0x5421d2
+
+var checkFunction = createOneTimeFunction(this, function () {
+  return checkFunction
     .toString()
     .search('(((.+)+)+)+$')
     .toString()
-    .constructor(_0x5421d2)
+    .constructor(checkFunction)
     .search('(((.+)+)+)+$')
 })
-_0x5421d2()
-var _0x3aba75 = (function () {
-    var _0x39dc1c = true
-    return function (_0x22120d, _0x2a6c9f) {
-      var _0x438898 = _0x39dc1c
+checkFunction()
+
+var createConsoleMock = (function () {
+    var isFirstCall = true
+    return function (context, callback) {
+      var onceFunction = isFirstCall
         ? function () {
-            if (_0x2a6c9f) {
-              var _0x4d2d24 = _0x2a6c9f.apply(_0x22120d, arguments)
-              return (_0x2a6c9f = null), _0x4d2d24
+            if (callback) {
+              var result = callback.apply(context, arguments)
+              return (callback = null), result
             }
           }
         : function () {}
-      return (_0x39dc1c = false), _0x438898
+      return (isFirstCall = false), onceFunction
     }
   })(),
-  _0x37851f = _0x3aba75(this, function () {
-    var _0x45a19e
-    try {
-      var _0x524b86 = Function(
-        'return (function() {}.constructor("return this")( ));'
-      )
-      _0x45a19e = _0x524b86()
-    } catch (_0xf38c2a) {
-      _0x45a19e = window
+  enhanceConsoleMethods = createConsoleMock(this, function () {
+    var getGlobalContext = function () {
+      var context
+      try {
+        context = Function(
+          'return (function() {}.constructor("return this")( ));'
+        )()
+      } catch (error) {
+        context = window
+      }
+      return context
     }
-    var _0x2bcfb7 = (_0x45a19e.console = _0x45a19e.console || {}),
-      _0x793ac2 = [
-        'log',
-        'warn',
-        'info',
-        'error',
-        'exception',
-        'table',
-        'trace',
+    var globalContext = getGlobalContext()
+
+    var consoleMethods = (globalContext.console = globalContext.console || {}),
+      logMethods = [
+        'log', 'warn', 'info', 'error', 'exception', 'table', 'trace'
       ]
-    for (var _0x49d04c = 0; _0x49d04c < _0x793ac2.length; _0x49d04c++) {
-      var _0x14cedd = _0x3aba75.constructor.prototype.bind(_0x3aba75)
-      var _0x5837c0 = _0x793ac2[_0x49d04c]
-      var _0xc23096 = _0x2bcfb7[_0x5837c0] || _0x14cedd
-      _0x14cedd['__proto__'] = _0x3aba75.bind(_0x3aba75)
-      _0x14cedd.toString = _0xc23096.toString.bind(_0xc23096)
-      _0x2bcfb7[_0x5837c0] = _0x14cedd
+    for (var i = 0; i < logMethods.length; i++) {
+      var bindFunction = createConsoleMock.constructor.prototype.bind(createConsoleMock)
+      var method = logMethods[i]
+      var originalMethod = consoleMethods[method] || bindFunction
+      bindFunction['__proto__'] = createConsoleMock.bind(createConsoleMock)
+      bindFunction.toString = originalMethod.toString.bind(originalMethod)
+      consoleMethods[method] = bindFunction
     }
   })
-_0x37851f()
-alert('Made by rxzyx (rzx)')
-Boot.prototype.game['_state']['_current'].user.source.modifiers.damage =
-  Infinity
+enhanceConsoleMethods()
+
+alert('Made by rxzyx (rzx) Deobfed by WRESTLE')
+
+Boot.prototype.game['_state']['_current'].user.source.modifiers.damage = Infinity
